a013565909a8976df89db2833dac889c851995f0
Simon Abraham <nomis223@gmail.com>
2018-02-19 15:37:29 +0100

    fixing assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  # Verifies that versions and hashed value of the package contents in the project's package.json
+  config.webpacker.check_yarn_integrity = true
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -26,6 +29,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
