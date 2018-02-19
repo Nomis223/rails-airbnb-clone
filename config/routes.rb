751aeb94f07d7feec7c8f4964da7b8195bc08fd6
Simon Abraham <nomis223@gmail.com>
2018-02-19 18:44:55 +0100

    controller for guitar and user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/destroy'
+
+  get 'guitars/index'
+
+  get 'guitars/show'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
