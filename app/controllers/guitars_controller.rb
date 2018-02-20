eb7a984828451dc88451bad6be923c0d47017d85
Marc Wyss <wyss.a.marc@gmail.com>
2018-02-20 13:54:19 +0100

    Basic layout of index page complete

--- app/controllers/guitars_controller.rb
+++ app/controllers/guitars_controller.rb
@@ -1,5 +1,10 @@
 class GuitarsController < ApplicationController
   def index
+    if params[:query].present?
+      @guitars = Guitar.where("brand ILIKE ? AND address ILIKE ?", "%#{params[:brand]}%", "%#{params[:address]}%")
+    else
+      @guitars = Guitar.all
+    end
   end
 
   def show
