a89ffb7564df657f574c9dedb68528dbf124bcd0
Marc Wyss <wyss.a.marc@gmail.com>
2018-02-21 11:08:57 +0100

    Search bar works!

--- app/controllers/guitars_controller.rb
+++ app/controllers/guitars_controller.rb
@@ -3,7 +3,7 @@ class GuitarsController < ApplicationController
   before_action :set_guitar, only:[:show]
 
   def index
-    if params[:query].present?
+    if params[:brand].present? && params[:address].present?
       @guitars = policy_scope(Guitar).where("brand ILIKE ? AND address ILIKE ?", "%#{params[:brand]}%", "%#{params[:address]}%")
     else
       @guitars = policy_scope(Guitar).all
