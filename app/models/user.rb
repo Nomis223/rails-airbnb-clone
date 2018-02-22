a48858c9edb38560f066064fe016f720d5920c76
Simon Abraham <nomis223@gmail.com>
2018-02-22 14:33:26 +0100

    feat: profile daashboard logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :guitars, dependent: :destroy
-  #lajdflajfdalsfjaslkfdjalfjal do something
+  has_many :bookings, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  get '/users/:id', to: 'users#show', as: :user
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :guitars do
