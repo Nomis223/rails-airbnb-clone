d64d3bc0547dbcebbe78da9a377a9a7927f7095e
Simon Abraham <nomis223@gmail.com>
2018-02-22 15:11:58 +0100

    fixed price has been implemented

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,13 @@
 class Booking < ApplicationRecord
   belongs_to :guitar
   belongs_to :user
+  before_validation :fixed_price
+
+
+  def fixed_price
+  price = self.guitar.price_per_day
+  beg = Date.parse(self.starts_at)
+  fin = Date.parse(self.ends_at)
+  self.fixed_price = (fin - beg) * price
+  end
 end
